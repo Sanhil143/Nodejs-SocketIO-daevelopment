10b07b22d0202c2bc5ee85caac207cf33f2d06c6
Sanhil143 <sanhilrai143@gmail.com>
2023-05-27 17:46:29 +0530

    socket setup successfully

--- 01-Setup/src/server.js
+++ 01-Setup/src/server.js
@@ -1,7 +1,31 @@
-const express = require('express');
-const io = require('socket.io');
-const http = require('http');
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const path = require('path');
+
+
+app.get('/', (req,res) => {
+      var options = {
+            root: path.join(__dirname)
+      }
+
+      var fileName = 'index.html';
+      res.sendFile(fileName,options)
+})
+
+io.on('connection', (socket) => {
+      console.log("Socket is connected");
+
+      socket.on('disconnect', () => {
+            console.log('Socket is disconnected');
+      })
+})
+
+server.listen(3000,() => {
+      console.log('server is running on port 3000');
+})
 
 
 
-const app = express();
+
+
